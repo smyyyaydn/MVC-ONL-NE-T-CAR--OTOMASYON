132a6ec5cad1e8012547894f399ed4428ba3308e
Sümeyye <52049640+smyyyaydn@users.noreply.github.com>
2022-10-15 20:18:39 +0300

    .

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/DepartmanController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departmans.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman d)
+        {
+            c.Departmans.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Sınıflar/Departman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Departman
+    {
+        [Key]
+        public int Depertmanid { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string DepertmanAd { get; set; }
+        public bool Durum { get; set; }
+        public ICollection<Personel> Personels { get; set; }
+    }
+}
